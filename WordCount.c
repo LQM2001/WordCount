4c9395c09c3a01d52d7374270e79d8d24934357d
unknown <2360137088@qq.com>
2021-05-08 21:27:09 +0800

    bb

---
+++ WordCount.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+void git(char *filename){
+	FILE *fp;
+	char buffer[1000];
+	char c;
+	int bufferlen;
+	int isLastBlank = 0; 
+	int charCount=0;
+	int wordCount=0;
+	int i;
+ 
+	if((fp=fopen(filename,"rb"))==NULL){
+		printf("文件不能打开\n");
+		exit(0);
+	}
+	while(fgets(buffer,1000,fp)!=NULL){
+		bufferlen=strlen(buffer);
+		for(i=0;i<bufferlen;i++){
+			c=buffer[i];
+			if(c==' ' || c=='\t'){
+				if(isLastBlank==0){
+					wordCount++;
+				}
+				isLastBlank=1;
+			}else if(c!='\n'&&c!='\r'){
+				charCount++;
+				isLastBlank=0;
+			}
+ 
+		}
+		if(isLastBlank==0)
+			wordCount++;
+		isLastBlank=1;
+	}
+	fclose(fp);
+	printf("字符数：%d\n",charCount);
+	printf("单词数：%d\n",wordCount);
+ 
+	
+}
+void main(){
+	char filename[]="D://WordCount//input.txt";
+	git(filename);
+}
